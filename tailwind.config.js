0996fc0e080e09826a07340fc62da25ef0da6371
Michael Sinkamba <michaelsinkamba211@gmail.com>
2025-12-01 06:25:14 +0200

    removed dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
-  darkMode: 'class',
+  // darkMode: 'class',
   plugins: [require('@tailwindcss/typography')],
   theme: {
     fontSize: {
